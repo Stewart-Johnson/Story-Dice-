0111643bd643700c3f4c1db70f7bd5527c6a72d1
ragonz17 <ragonz17@asu.edu>
2016-04-29 16:17:45 -0700

    Update drawsImage.java
    Final full documentation

--- StoryDice/src/drawsImage.java
+++ StoryDice/src/drawsImage.java
@@ -6,12 +6,24 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*; 
+/** 
+ * drawsImage sets an image to be used in the Main GUI
+ * 
+ * @author Ryan Gonzales
+ * 
+ * @version April 29, 2016
+ * 
+ */
 
 public class drawsImage extends JPanel
 {
     private static final long serialVersionUID = 1L;
     private BufferedImage image;
-  
+    
+    /**
+     * Constructor creates the image
+     * @parameter a string from the roll with the file location for the image to be drawn
+     */
     public drawsImage(String imageStr) 
     {
         try 
@@ -24,7 +36,9 @@ public class drawsImage extends JPanel
             e.printStackTrace();
         }
     } 
-  
+    /**
+    * Simple paint component using image from constructor 
+     */
     @Override
     public void paintComponent(Graphics g)
     {
