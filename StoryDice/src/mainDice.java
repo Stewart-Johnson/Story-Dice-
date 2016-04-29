2dffeba78f52b7320841f1636721ba4f5e060e91
ragonz17 <ragonz17@asu.edu>
2016-04-29 15:01:45 -0700

    Update mainDice.java
    Added Images with words to high score tracking

--- StoryDice/src/mainDice.java
+++ StoryDice/src/mainDice.java
@@ -1,4 +1,4 @@
-package storydice;
+
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
@@ -11,6 +11,11 @@ public class mainDice
 	static int numRounds = 1;
 	public static void main(String[] args)
 	{
+		System.out.println("Welcome to Story Dice!");
+		System.out.println("You need at least two people in order to play this game!");
+		System.out.println("Write a story based on the 3 pictures you roll and then rate your friends stories!");
+		System.out.println("The person with the highest average score at the end wins!");
+
 		
 		// show high scores
 		
@@ -25,8 +30,7 @@ public class mainDice
 
         try {
             // FileReader reads text files in the default encoding.
-            FileReader fileReader = 
-                new FileReader(fileName);
+            FileReader fileReader =  new FileReader(fileName);
 
             // Always wrap FileReader in BufferedReader.
             BufferedReader bufferedReader = 
@@ -50,7 +54,6 @@ public class mainDice
             // Or we could just do this: 
             // ex.printStackTrace();
         }
-		
 		//get number of players from console
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter number of players");
@@ -85,27 +88,22 @@ public static void playGame(Player[] player, int numPlayers, int numRounds)
 		for (int index = 0;index <= player.length-1;index ++)
 		{
 			System.out.println(player[index].getName() + "'s turn");
-			storyPics[index] = new Dice(true);
+			storyPics[index] = new Dice();
 			JFrame window = new JFrame("Your Dice Roll");
 			GridLayout pictureGrid  = new GridLayout(0,3);
 			window.setSize(900, 300);
 			window.setLayout(pictureGrid);
-	        window.add(new drawsImage(storyPics[index].getRoll(0)));
-	        window.add(new drawsImage(storyPics[index].getRoll(1)));
-	        window.add(new drawsImage(storyPics[index].getRoll(2)));
+	        window.add(new drawsImage(storyPics[index].getRollImages(0)));
+	        window.add(new drawsImage(storyPics[index].getRollImages(1)));
+	        window.add(new drawsImage(storyPics[index].getRollImages(2)));
 	        window.setLocationRelativeTo(null);
 	        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        window.setVisible(true); 
+	        System.out.println(Arrays.toString(storyPics[index].getRollWords()));
 			System.out.println("What's the Story? \n");
 			String story = scannedStory.nextLine();
 			player[index].setStory(story);
 			window.setVisible(false);
-		}
-		for (int index = 0; index <= player.length-1;index++)
-		{
-			int highcount = index+1;
-			int lowcount = index -1;
-			System.out.println(Arrays.toString(storyPics[index].getRoll()));
 		}
 		
 		//allow for ratings of the stories
@@ -122,12 +120,13 @@ public static void playGame(Player[] player, int numPlayers, int numRounds)
 					GridLayout pictureGrid  = new GridLayout(0,3);
 					window.setSize(900, 300);
 					window.setLayout(pictureGrid);
-			        window.add(new drawsImage(storyPics[rate].getRoll(0)));
-			        window.add(new drawsImage(storyPics[rate].getRoll(1)));
-			        window.add(new drawsImage(storyPics[rate].getRoll(2)));
+			        window.add(new drawsImage(storyPics[rate].getRollImages(0)));
+			        window.add(new drawsImage(storyPics[rate].getRollImages(1)));
+			        window.add(new drawsImage(storyPics[rate].getRollImages(2)));
 			        window.setLocationRelativeTo(null);
 			        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			        window.setVisible(true); 
+			        System.out.println(Arrays.toString(storyPics[index].getRollWords()));
 					System.out.println(player[rate].getStory());
 					System.out.println("1 - 5 how would you rate this story");
 					int score = scannedStory.nextInt();
@@ -190,33 +189,6 @@ public static void playGame(Player[] player, int numPlayers, int numRounds)
 	        }
 			
 			return;
-		}
-	}	
-			
-			
-}	
-				
-	
-	
-	//menu to start new game, quit or see high scores
-		
-		//start new game
-			//ask user to for number of players playing the game
-				//create array of objects "players" from player class with size being number of players
-					
-				//for loop that gets player names
-				
-				//for loop that displays images and gets story for each player
-
-				//for loop that displays stories and images from other players to each player and sets score for each
-				
-				//save and sort high scores to text file
-				
-				//ask player if another round y or n
-				
-				//n = exit program y = return to image and story for loop
-		
-		// quit exit program
-		
-		// high scores prints names and scores from text file
-
+		}	
+}
+}
