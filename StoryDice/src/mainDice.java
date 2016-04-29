789bbc418faa3ef0421b6021116f9b6a1b9ec63b
wamsize <sifriedm@asu.edu>
2016-04-29 16:14:40 -0700

    Updated mainDice Class
    Added documentation to the class.

--- StoryDice/src/mainDice.java
+++ StoryDice/src/mainDice.java
@@ -1,38 +1,52 @@
-
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
 import java.io.*;
+
+/** 
+ * The mainDice Class provides the main method for our program and is what should be run
+ * to initialize and play the game.
+ * 
+ * @author Samuel Friedman, Ryan Gonzalez, Stewart Johnson
+ * 
+ * @version April 29, 2016
+ * 
+ */
 
 public class mainDice
 {
 	
 	static int count = 1;
 	static int numRounds = 1;
+	
+	/**
+	 * Main Method runs through the game.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
+		
+		// Introduction to the game.
+		
 		System.out.println("Welcome to Story Dice!");
 		System.out.println("You need at least two people in order to play this game!");
 		System.out.println("Write a story based on the 3 pictures you roll and then rate your friends stories!");
 		System.out.println("The person with the highest average score at the end wins and gets a place in the Hall of Winners!");
-
 		
-		// show high scores
+		// Show previous winners by reading from winners.txt if it exists.
 		
 		System.out.println("Hall of Winners:");
 		System.out.println();
 		
-		// The name of the file to open.
-        String fileName = "src/storydice/winners.txt";
+        String fileName = "src/winners.txt";
 
-        // This will reference one line at a time
         String line = null;
 
         try {
-            // FileReader reads text files in the default encoding.
+        	
             FileReader fileReader =  new FileReader(fileName);
 
-            // Always wrap FileReader in BufferedReader.
             BufferedReader bufferedReader = 
                 new BufferedReader(fileReader);
 
@@ -40,33 +54,34 @@ public class mainDice
                 System.out.println(line);
             }   
 
-            // Always close files.
             bufferedReader.close();
             System.out.println();
         }
         catch(FileNotFoundException ex) {
-                          
+        	// Avoid any error messages if the file doesn't exist yet.
         }
-        catch(IOException ex) {
-            System.out.println(
-                "Error reading file '" 
-                + fileName + "'");                  
-            // Or we could just do this: 
-            // ex.printStackTrace();
+        catch(IOException ex) {                
+            ex.printStackTrace();
         }
-		//get number of players from console
+        
+		// Get number of players from console.
+        
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter number of players");
 		int numPlayers = sc.nextInt();
-		//create new player array
+		
+		// Create a player object array to story the players.
+		
 		Player [] player = new Player[numPlayers];
-		//instantiate player array
+		
+		// Instantiate player array with new players. 
+		
 		for(int index = 0;index <= player.length-1;index++)
 		{
 			player[index] = new Player();
 		}
 		
-		//get player names
+		// Get the names of the players.
 		
 		for(int index = 0;index <= player.length-1;index ++)
 		{	
@@ -75,18 +90,36 @@ public class mainDice
 			String name = sc.next();
 			player[index].setName(name);
 			count++;
-		}
+		}		
 		
+		// Start the game.
 		
 		playGame(player, numPlayers, numRounds);
-	}
 		
-public static void playGame(Player[] player, int numPlayers, int numRounds)
-{
+	}
+	
+	/**
+	 * Once all information has been gathered from the players, begin playing the game. This
+	 * class involves showing and retrieving stories, playing new rounds, and saving to the
+	 * hall of winners.
+	 * 
+	 * @param player Array of players for the game
+	 * @param numPlayers The number of players
+	 * @param numRounds The number of rounds; this is incremented when the group wants to play another round
+	 */
+	
+	public static void playGame(Player[] player, int numPlayers, int numRounds)
+	{
+		
+		// Create Scanner and Dice array objects.
+		
 		Scanner scannedStory=new Scanner(System.in);
-		Dice [] storyPics = new Dice[numPlayers];
-		for (int index = 0;index <= player.length-1;index ++)
-		{
+		Dice[] storyPics = new Dice[numPlayers];
+		
+		// Show and retrieve the stories for each player.
+		
+		for (int index = 0;index <= player.length-1;index ++) {
+			
 			System.out.println(player[index].getName() + "'s turn");
 			storyPics[index] = new Dice();
 			JFrame window = new JFrame("Your Dice Roll");
@@ -104,17 +137,21 @@ public static void playGame(Player[] player, int numPlayers, int numRounds)
 			String story = scannedStory.nextLine();
 			player[index].setStory(story);
 			window.setVisible(false);
+			
 		}
 		
-		//allow for ratings of the stories
-		for(int index = 0; index < numPlayers; index++)
-		{
-			for(int rate = 0; rate < numPlayers; rate++)
-			{
-				if(index == rate)
+		// Loop through each player and allow them to rate all the other stories.
+		
+		for(int index = 0; index < numPlayers; index++) {
+			
+			for(int rate = 0; rate < numPlayers; rate++) {
+				
+				if(index == rate) {
+					
 					System.out.println("\n");
-				else
-				{
+				
+				} else {
+					
 					System.out.println(player[index].getName()+" Please rate these stories ");	
 					JFrame window = new JFrame("Your Dice Roll");
 					GridLayout pictureGrid  = new GridLayout(0,3);
@@ -135,53 +172,59 @@ public static void playGame(Player[] player, int numPlayers, int numRounds)
 				}
 			}
 		}
-		for(int index = 0; index < numPlayers; index++)
-		{
+		
+		// Display all scores to the users.
+		
+		for (int index = 0; index < numPlayers; index++) {
+			
 			player[index].setCount(numRounds, numPlayers);
 			System.out.println(player[index].getName() + "'s Score is: " + Double.toString(player[index].getScore()));
+		
 		}
+		
+		// Ask to player another round.
 		
 		System.out.println("Play another round? Y/N ");
 		String nextRound = scannedStory.next();
-		if(nextRound.equals("Y"))
-		{
+		
+		if(nextRound.equals("Y")) {
+			
+			// Increment number of rounds and replay.
+			
 			numRounds++;
 			System.out.println("Let's play again!");
 			playGame(player, numPlayers, numRounds);
-		}
-		else
-		{
+			
+		} else {
+			
+			// Display final scores for all rounds.
+			
 			System.out.println("Final Score is: ");
 			int max = 0;
-			for(int index = 0; index < numPlayers; index++)
-			{
+			for(int index = 0; index < numPlayers; index++) {
 				if (player[index].getScore() > max) {
 					max = index;
 				}
 				player[index].setCount(numRounds, numPlayers);
 				System.out.println(player[index].getName() + "'s Score is: " + Double.toString(player[index].getScore()));
 			}
+						
 			
-			// file
+			// Install the victor into the immortalized hall of winners.
 			
-			 // The name of the file to open.
-	        String fileName = "src/storydice/winners.txt";
+	        String fileName = "src/winners.txt";
 
 	        try {
-	            // Assume default encoding.
+	        	
 	            FileWriter fileWriter =
 	                new FileWriter(fileName, true);
 
-	            // Always wrap FileWriter in BufferedWriter.
 	            BufferedWriter bufferedWriter =
 	                new BufferedWriter(fileWriter);
 
-	            // Note that write() does not automatically
-	            // append a newline character.
 	            bufferedWriter.write(player[max].getName() + ": " + player[max].getScore());
 	            bufferedWriter.newLine();
 
-	            // Always close files.
 	            bufferedWriter.close();
 	        }
 	        catch(IOException ex) {
@@ -190,5 +233,5 @@ public static void playGame(Player[] player, int numPlayers, int numRounds)
 			
 			return;
 		}	
-}
+	}
 }
