138fb4fac4d36405ed408035e2ba96542876c8b9
ragonz17 <ragonz17@asu.edu>
2016-04-29 14:57:24 -0700

    Update Dice.java
    Words with Images

--- StoryDice/src/Dice.java
+++ StoryDice/src/Dice.java
@@ -1,10 +1,11 @@
 import java.util.*;
 public class Dice 
 {
-	private String[] Roll;
-	private String[] dice1t = {"dog", "flower", "hot air balloon", "airplane" , "worry", "sheep"};
-	private String[] dice2t = {"pyramids", "think", "house", "clock", "bee", "books"};
-	private String[] dice3t = {"fish", "magic", "bugs", "lock", "fire", "question"};
+	private String[] RollImages;
+	private String[] RollWords;
+	private String[] dice1t = {"dog", "flower", "hot air balloon", "airplane" , "sheep", "pyramid"};
+	private String[] dice2t = {"think", "house", "clock", "bee", "book", "fish"};
+	private String[] dice3t = {"bug", "lock", "fire", "question", "compass", "key"};
 	private String[] dice1g = {"src/d1image1.jpg","src/d1image2.jpg","src/d1image3.jpg","src/d1image4.jpg","src/d1image5.jpg","src/d1image6.jpg"};
 	private String[] dice2g = {"src/d2image1.jpg","src/d2image2.jpg","src/d2image3.jpg","src/d2image4.jpg","src/d2image5.jpg","src/d2image6.jpg"};
 	private String[] dice3g = {"src/d3image1.jpg","src/d3image2.jpg","src/d3image3.jpg","src/d3image4.jpg","src/d3image5.jpg","src/d3image6.jpg"};
@@ -16,32 +17,38 @@ public class Dice
 	 *@param gooey is a boolean that tells the game to either initialize a text based or gui based roll
 	 */
 	
-	public Dice(boolean gooey)
+	public Dice()
 	{
-		Roll = new String[3];
+		RollImages = new String[3];
+		RollWords = new String[3];
 		Random r = new Random();
-		if(gooey == false){
-			Roll[0] = dice1t[r.nextInt(6)];
-			Roll[1] = dice2t[r.nextInt(6)];
-			Roll[2] = dice3t[r.nextInt(6)];
-		}else{
-			Roll[0] = dice1g[r.nextInt(6)];
-			Roll[1] = dice2g[r.nextInt(6)];
-			Roll[2] = dice3g[r.nextInt(6)];
-		}
+		int rand1 = r.nextInt(6);
+		int rand2 = r.nextInt(6);
+		int rand3 = r.nextInt(6);
+		RollImages[0] = dice1g[rand1];
+		RollImages[1] = dice1g[rand2];
+		RollImages[2] = dice1g[rand3];
+		RollWords[0] = dice1t[rand1];
+		RollWords[1] = dice1t[rand2];
+		RollWords[2] = dice1t[rand3];
 		
 	}
 	/**
 	 * This simple getter method returns the roll
 	 * @return returns the string array of the roll results for use by other classes
 	 */
-	public String[] getRoll(){
-		return Roll;
+	public String[] getRollImages(){
+		return RollImages;
 	}
 	
-	public String getRoll(int index)
+	public String[] getRollWords()
 	{
-		return Roll[index];
+		return RollWords;
+	}
+	
+	public String getRollImages(int index)
+	{
+		return RollImages[index];
 	}
 	
 	
