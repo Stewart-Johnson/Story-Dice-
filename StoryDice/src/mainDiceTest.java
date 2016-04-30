4409364faa551c6d257bd65fc9e5b1cc9fcdc5b6
Stewart Johnson <sajohn32@asu.edu>
2016-04-29 18:20:00 -0700

    maindiceTest

---
+++ StoryDice/src/mainDiceTest.java
@@ -0,0 +1,17 @@
+package Stewart;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class mainDiceTest 
+{
+	
+	@Test
+	public void testMainDice()
+	{
+		mainDice main = new mainDice();
+		assertNotNull(main);
+	}
+
+}
